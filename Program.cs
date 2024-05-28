7418adbe9bec0986796f25cb946075ad830df95d
RJ Nay <rjnay1984@gmail.com>
2024-05-28 15:50:58 -0500

    hook authentik to api

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthentication().AddJwtBearer("Authentik");
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapGet("/", () => new { Message = "Hello World!"});
+app.MapGet("/protected", () => new { Message = "Protected route"}).RequireAuthorization();
 
 app.Run();
